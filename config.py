b69132c62576a674be84df8a767698f830cf0191
zenogoku123 <160753991+zenogoku123@users.noreply.github.com>
2024-05-02 08:19:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ API_HASH = os.environ.get("API_HASH", "365f3421c4243310e1c738e2b6fb63a1")
 
 OWNER = os.environ.get("OWNER", "https://t.me/Grand_Zeno_Omni_KingBot") #Owner username
 OWNER_ID = int(os.environ.get("OWNER_ID", "5806054139")) #Owner user id
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://gokumuikaioken20times:@qazplmwsxoknABC123@cluster0.4ed1fpj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://frankanimes:qayplmwsxokn@cluster0.xksjqdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 
